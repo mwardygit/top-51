92f58a4337b4f9fdefd0007ca9a29833e0112c88
Mike Ward <mward4830@gmail.com>
2023-11-13 16:42:39 -0800

    nav bar style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import DropMenu from './DropMenu'
+import vinylPic from '../goldRecord.jpg'
+
+
 
 // Creates the top portion of the main page
 // receives: props.onChange (function)
@@ -9,20 +12,19 @@ import DropMenu from './DropMenu'
 const Header = (props) => {
    return (
       <div id='top-nav-bar'>
-      <div id='title-area'>
-         <h1>Top 51 Artists</h1>
-      </div>
-      <div id='genre-menu'>
-         <p>Filter by Genre:</p>
-         <DropMenu
-            onChange={props.onChange}
-            allGenres={props.allGenres}
-         />
-         <div id='genre-matches'>
-            Count: {props.currentMatches}
+         <div id='main-title'>
+            <img src={vinylPic}/>
+            <h1>Top 51 Artists</h1>
+         </div>
+         <div id='genre-menu'>
+            <h3>Filter:
+            <DropMenu
+               onChange={props.onChange}
+               allGenres={props.allGenres}
+            /></h3>
+            <p>{'- ' + props.currentMatches + ' -'}</p>
          </div>
       </div>
-   </div>
    )
 }
 
